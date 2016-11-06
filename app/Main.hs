537efe864d3a8f1e1ae0130eb37806fb8875efb8
shawa <andrewshaw.mail@gmail.com>
2016-11-06 17:05:51 +0000

    Really bad at this

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Web.Scotty
+import qualified Web.Scotty as S
 
-main = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!"
+import Text.Blaze.Html5 hiding (main)
+import Text.Blaze.Html5.Attributes
+import Text.Blaze.Html.Renderer.Text
+
+
+response name = do renderHtml $ do
+                     h1 ("Hello " >> toHtml name)
+
+
+main = S.scotty 3000 $ do
+  S.get "/" $ do
+    S.html "Hello World!"
+
+  S.get "/greet/:name" $ do
+    name <- S.param "name"
+    S.html $ response name
