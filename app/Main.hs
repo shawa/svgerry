9d6bc9ca298e8f506f6026bd47fe935235f4630a
shawa <andrewshaw.mail@gmail.com>
2016-11-09 15:16:52 +0000

    add front end

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,7 @@ import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
 import Text.Blaze.Html.Renderer.Text
-import Text.Blaze.Svg.Renderer.Utf8
+import Text.Blaze.Svg.Renderer.Text
 
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text as T
@@ -37,4 +37,4 @@ main = scotty 3000 $ do
       shapeText <- (S.param "shapeText") `rescue` return
       S.setHeader "Content-Type" "image/svg+xml"
       let shape = shapeFromText $ (T.unpack . TL.toStrict) shapeText :: Shape
-      S.raw $ toSvg [(identity, shape)]
+      S.text $ renderSvg $ toSvg [(identity, shape)]
