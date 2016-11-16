661b937310c6dabae9e1ce83b55a80afe79bdc12
shawa <andrewshaw.mail@gmail.com>
2016-11-16 13:29:27 +0000

    Add style from strigAttribute

--- src/Shapes/Interpret.hs
+++ src/Shapes/Interpret.hs
@@ -4,8 +4,7 @@ module Shapes.Interpret where
 import qualified Data.Matrix as M
 import Control.Monad
 
-import Text.Blaze.Svg11 ((!), Svg)
-import qualified Text.Blaze.Svg11 as S
+import Text.Blaze.Svg11
 import Text.Blaze.Svg11.Attributes
 
 import Shapes.Lang
@@ -23,33 +22,33 @@ getMatrix [t] = M.fromLists $ case t of Identity      -> [[1, 0, 0] ,[0, 1, 0] ,
 
 getMatrix (t:ts) = (getMatrix [t]) `M.multStd` (getMatrix ts)
 
-toSvgElem :: Shape -> S.Svg
-toSvgElem Empty  = S.circle ! r "0"
-toSvgElem Circle = S.circle ! r "1"
-toSvgElem Square = S.rect   ! width  "10" ! height "10"
+toSvgElem :: Shape -> Svg
+toSvgElem Empty  = circle ! r "0"
+toSvgElem Circle = circle ! r "1"
+toSvgElem Square = rect   ! width  "10" ! height "10"
 
 
-toAttr :: Style -> S.Attribute
-toAttr (StrokeWidth w) = strokeWidth "0.1"
-toAttr (StokeColour c) = stroke "red"
-toAttr (FillColour c)  = fill "green"
+toAttr :: Style -> Attribute
+toAttr (StrokeWidth  w) = strokeWidth $ stringValue $show w
+toAttr (StrokeColour c) = stroke      $ stringValue $ show c
+toAttr (FillColour   c) = fill        $ stringValue $ show c
 
 -- https://developer.mozilla.org/en/docs/Web/SVG/Attribute/transform
 -- We've had 3x3 matrices so far; in order to compose transformations easily
 -- SVG's matrix constructor only takes 6 arguments, as the last row is always [0 0 1]
-toAttrs t = transform $ S.matrix a b c d e f where [ [a, c, e] ,[b, d, f] ,[0, 0, 1]] = M.toLists $ getMatrix t
+toAttrs t = transform $ matrix a b c d e f where [ [a, c, e] ,[b, d, f] ,[0, 0, 1]] = M.toLists $ getMatrix t
 
 
-toSvg :: Figure -> S.Svg
+toSvg :: Figure -> Svg
 toSvg (styles, transforms, shape) = foldl (!) (toSvgElem shape) $ [toAttrs transforms] ++ map toAttr styles
 
-toSvgs :: [Figure] -> S.Svg
+toSvgs :: [Figure] -> Svg
 toSvgs []         = toSvgElem Empty
 toSvgs [fig]      = toSvg fig
 toSvgs (fig:figs) = toSvg fig >> toSvgs figs
 
 
-toSvgDoc :: [Figure] -> S.Svg
+toSvgDoc :: [Figure] -> Svg
 toSvgDoc figs = svgHead $ toSvgs figs
 
-svgHead = S.docTypeSvg ! version "1.1" ! width "1200" ! height "550" ! viewbox "0 0 3 2"
+svgHead = docTypeSvg ! version "1.1" ! width "1200" ! height "550" ! viewbox "0 0 3 2"
