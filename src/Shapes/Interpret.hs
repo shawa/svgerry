f64df679d4331c18989c90ea8f8d0f20a690c438
shawa <andrewshaw.mail@gmail.com>
2016-11-17 16:08:40 +0000

    Do some nice cleanup and refactoring, <3 types

--- src/Shapes/Interpret.hs
+++ src/Shapes/Interpret.hs
@@ -1,26 +1,24 @@
 {-# LANGUAGE OverloadedStrings#-}
 module Shapes.Interpret where
 
-import qualified Data.Matrix as M
-import Control.Monad
+import Data.Matrix (Matrix, multStd, fromLists, toLists, identity)
 
 import Text.Blaze.Svg11
-import Text.Blaze.Svg11.Attributes
+import Text.Blaze.Svg11.Attributes hiding (multStd)
 
 import Shapes.Lang
 
-type Point = M.Matrix Double
+matList :: Transform -> [[Double]]
+matList (Identity)      = [[1, 0, 0] ,[0, 1, 0] ,[0, 0, 1]]
+matList (Scale y x)     = [[x, 0, 0] ,[0, y, 0] ,[0, 0, 1]]
+matList (Translate y x) = [[1, 0, x] ,[0, 1, y] ,[0, 0, 1]]
+matList (Rotate a')     = [[cos a, -sin a, 0], [sin a, cos a, 0], [0, 0, 1]]
+                          where a       = deg2rad a'
+                                deg2rad = (* (pi/180))
 
-getMatrix :: [Transform] -> M.Matrix Double
-getMatrix [] = M.identity 3
-getMatrix [t] = M.fromLists $ case t of Identity      -> [[1, 0, 0] ,[0, 1, 0] ,[0, 0, 1]]
-                                        Scale y x     -> [[x, 0, 0] ,[0, y, 0] ,[0, 0, 1]]
-                                        Translate y x -> [[1, 0, x] ,[0, 1, y] ,[0, 0, 1]]
-                                        Rotate a'     -> [[(cos a), (-sin a), 0] ,[(sin a), (cos a) , 0] ,[0, 0, 1]]
-                                         where a       = deg2rad a'
-                                               deg2rad = (* (pi/180))
-
-getMatrix (t:ts) = (getMatrix [t]) `M.multStd` (getMatrix ts)
+getMatrix :: [Transform] -> Matrix Double
+getMatrix ts = foldl multStd (identity 3) (map mat ts)
+               where mat = (fromLists . matList)
 
 toSvgElem :: Shape -> Svg
 toSvgElem Empty  = circle ! r "0"
@@ -29,26 +27,28 @@ toSvgElem Square = rect   ! width  "10" ! height "10"
 
 
 toAttr :: Style -> Attribute
-toAttr (StrokeWidth  w) = strokeWidth $ stringValue $show w
-toAttr (StrokeColour c) = stroke      $ stringValue $ show c
-toAttr (FillColour   c) = fill        $ stringValue $ show c
+toAttr (StrokeWidth  w) = strokeWidth $ stringAttrVal w
+toAttr (StrokeColour c) = stroke      $ stringAttrVal c
+toAttr (FillColour   c) = fill        $ stringAttrVal c
+
+stringAttrVal :: Show a => a -> AttributeValue
+stringAttrVal = stringValue . show
 
 -- https://developer.mozilla.org/en/docs/Web/SVG/Attribute/transform
 -- We've had 3x3 matrices so far; in order to compose transformations easily
 -- SVG's matrix constructor only takes 6 arguments, as the last row is always [0 0 1]
-toAttrs t = transform $ matrix a b c d e f where [ [a, c, e] ,[b, d, f] ,[0, 0, 1]] = M.toLists $ getMatrix t
+toAttrs t = transform $ matrix a b c d e f
+                        where [[a, c, e], [b, d, f], [0, 0, 1]] = toLists $ getMatrix t
+                                                      --nice sanity check
 
 
 toSvg :: Figure -> Svg
 toSvg (styles, transforms, shape) = foldl (!) (toSvgElem shape) $ [toAttrs transforms] ++ map toAttr styles
 
-toSvgs :: [Figure] -> Svg
-toSvgs []         = toSvgElem Empty
-toSvgs [fig]      = toSvg fig
-toSvgs (fig:figs) = toSvg fig >> toSvgs figs
-
-
 toSvgDoc :: [Figure] -> Svg
-toSvgDoc figs = svgHead $ toSvgs figs
-
-svgHead = docTypeSvg ! version "1.1" ! width "1200" ! height "550" ! viewbox "0 0 3 2"
+toSvgDoc figs = svgHead $ foldl (>>) (toSvgElem Empty) (map toSvg figs)
+                where svgHead = docTypeSvg
+                                ! version "1.1"
+                                ! width "1200"
+                                ! height "550"
+                                ! viewbox "0 0 3 2"
