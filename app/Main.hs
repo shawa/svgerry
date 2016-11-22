12137fa18d6c6a309ff10c25dea4015fe86518e6
shawa <andrewshaw.mail@gmail.com>
2016-11-22 16:08:03 +0000

    Make the app source a bit nicer to read

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,23 @@
 module Main where
 
 import Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
-import Data.Text.Lazy (toStrict)
+import Data.Text.Lazy (toStrict, Text)
 import Data.Text (unpack)
 import Shapes.Lang (Figure)
 import Shapes.Interpret (toSvgDoc)
-
+import Text.Read (readMaybe)
+import Network.HTTP.Types.Status (mkStatus)
 import Web.Scotty
+
+toShape :: Text -> Maybe [Figure]
+toShape = readMaybe . unpack . toStrict
 
 main = scotty 3000 $ do
     get "/" $ do file "./static/index.html"
 
     get "/svg" $ do
-      setHeader "Content-Type" "image/svg+xml"
       shapeText <- (param "shapeText") `rescue` return
-      let shapeDrawing = (read $ unpack $  toStrict shapeText) :: [Figure]
-      raw $ renderSvg $ toSvgDoc $ shapeDrawing
+      case toShape shapeText of Just figures -> do setHeader "Content-Type" "image/svg+xml"
+                                                   (raw . renderSvg . toSvgDoc) figures
+                                Nothing      -> do status $ mkStatus 406 "Failed to parse Shape specification"
+                                                   text "Failed to parse! Mismatched or missing parens?"
