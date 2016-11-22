c10971d6b7c73db204031b985ad138b762372db3
shawa <andrewshaw.mail@gmail.com>
2016-11-22 15:31:02 +0000

    Clean up imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 module Main where
+
+import Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
+import Data.Text.Lazy (toStrict)
+import Data.Text (unpack)
+import Shapes.Lang (Figure)
+import Shapes.Interpret (toSvgDoc)
+
 import Web.Scotty
-import Text.Blaze.Html5 hiding (main)
-import Text.Blaze.Html5.Attributes
-
-
-import qualified Web.Scotty as S
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-
-import Text.Blaze.Html.Renderer.Text
-import Text.Blaze.Svg.Renderer.Utf8
-
-import Text.Read hiding (get)
-
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text as T
-
-import Shapes.Lang
-import Shapes.Interpret
-
 
 main = scotty 3000 $ do
     get "/" $ do file "./static/index.html"
 
     get "/svg" $ do
-      S.setHeader "Content-Type" "image/svg+xml"
-      shapeText <- (S.param "shapeText") `rescue` return
-      let shapeDrawing = (read $ T.unpack $  TL.toStrict shapeText) :: [Figure]
-      S.raw $ renderSvg $ toSvgDoc $ shapeDrawing
+      setHeader "Content-Type" "image/svg+xml"
+      shapeText <- (param "shapeText") `rescue` return
+      let shapeDrawing = (read $ unpack $  toStrict shapeText) :: [Figure]
+      raw $ renderSvg $ toSvgDoc $ shapeDrawing
